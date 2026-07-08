bff3fe55f21d77ecd61962b92d47e23b93f40307
Rogério Barreto <rogeriooo.silva@gmail.com>
2026-07-07 21:24:04 -0300

    modelo de objeto de documento

---
+++ semana02/scripts/bom.js
@@ -0,0 +1,12 @@
+const input = document.querySelector("capfav");
+const input = document.querySelector("button");
+const input = document.querySelector("_____");
+
+const li = document.createElement("li");
+
+const botaoExcluir = document.createElement("button");
+
+li.textContent = input.value;
+botaoExcluir = textContent = "x";
+li.append(botaoExcluir);
+lista.append("li");
